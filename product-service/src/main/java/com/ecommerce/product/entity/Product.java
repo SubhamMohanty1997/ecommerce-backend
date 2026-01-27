08340cd0f8895aa209a2c12930aead5280ce626d
Subham Mohanty <subham087mohanty@gmail.com>
2026-01-27 18:04:43 +0530

    feat(product-service): added logging framework(Slf4j) and implemented Redis Cache to reduce latency

--- product-service/src/main/java/com/ecommerce/product/entity/Product.java
+++ product-service/src/main/java/com/ecommerce/product/entity/Product.java
@@ -1,11 +1,15 @@
 package com.ecommerce.product.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
@@ -14,13 +18,20 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Product name is mandatory")
     private String name;
+
+    @NotBlank(message = "Description is mandatory")
     private String description;
+
+    @NotNull(message = "Price cannot be null")
+    @Positive(message = "Price must be greater than 0")
     private BigDecimal price;
     //private Integer quantity;
     //private LocalDateTime localDateTime;
--- product-service/src/main/java/com/ecommerce/product/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/ecommerce/product/service/ProductServiceImpl.java
@@ -3,11 +3,16 @@ package com.ecommerce.product.service;
 import com.ecommerce.product.entity.Product;
 import com.ecommerce.product.exception.ProductNotFoundException;
 import com.ecommerce.product.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService{
     @Autowired
@@ -15,39 +20,52 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public Product createProduct(Product product) {
-        return productRepository.save(product);
+        Product savedProduct = productRepository.save(product);
+        log.info("Product created successfully with ID : {}",savedProduct.getId());
+        return savedProduct;
     }
-
+    @Cacheable(value = "products", key = "#id")
     @Override
     public Product getProductById(Long id) {
+        log.info("Fetching product with ID: {}",id);
         return productRepository.findById(id)
                 .orElseThrow(()->new ProductNotFoundException("Product not found with ID: "+id));
     }
 
     @Override
     public List<Product> getAllProducts() {
-        return productRepository.findAll();
+        log.info("Fetching all products");
+        List<Product> products = productRepository.findAll();
+        log.debug("Total products found={}",products.size());
+        return products;
     }
-
+    @CacheEvict(value = "products", key = "#id")
     @Override
     public void deleteProductById(Long id) {
+        log.info("Deleting product with ID: {}",id);
         Product product = productRepository.findById(id)
                 .orElseThrow(()->new ProductNotFoundException("Product not found with ID: "+id));
         productRepository.deleteById(id);
+        log.info("product deleted successfully with ID: {}",id);
     }
-
+    @CachePut(value = "products", key = "#result.id")
     @Override
     public Product updateProductById(Long id, Product product) {
+        log.info("product updating with new data of ID: {}",id);
         Product existing = productRepository.findById(id)
                 .orElseThrow(()->new ProductNotFoundException("Product not found with ID: "+id));
         existing.setId(product.getId());
         existing.setName(product.getName());
         existing.setDescription(product.getDescription());
         existing.setPrice(product.getPrice());
-        //existing.setQuantity(product.getQuantity());
-        //existing.setLocalDateTime(product.getLocalDateTime());
-
         Product updated = productRepository.save(existing);
+        log.info("product successfully updated of ID: {}",id);
         return updated;
+    }
+
+    @Override
+    public List<Product> createProducts(List<Product> products) {
+        List<Product> savedProducts = productRepository.saveAll(products);
+        return savedProducts;
     }
 }
