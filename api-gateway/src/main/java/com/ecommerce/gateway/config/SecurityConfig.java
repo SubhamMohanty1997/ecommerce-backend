cc82f8c53cdb708a34d3896febc457e2145f07ff
Subham Mohanty <subham087mohanty@gmail.com>
2026-02-17 16:53:02 +0530

    Implemented API-Gateway

---
+++ api-gateway/src/main/java/com/ecommerce/gateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+
+
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable())
+                .authorizeExchange(auth->auth.anyExchange().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
