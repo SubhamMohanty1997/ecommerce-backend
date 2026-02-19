244aff0db84e64d68044866c5b9f0760e53a4a37
Subham Mohanty <subham087mohanty@gmail.com>
2026-02-19 14:07:10 +0530

    fixed auth-service and api-gateway

--- api-gateway/src/main/java/com/ecommerce/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecommerce/gateway/config/SecurityConfig.java
@@ -16,8 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception{
         http.csrf(csrf->csrf.disable())
-                .authorizeExchange(auth->auth.anyExchange().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .authorizeExchange(auth->auth
+                        .pathMatchers("/oauth2/**").permitAll()
+                        .anyExchange().permitAll());
         return http.build();
     }
 }
